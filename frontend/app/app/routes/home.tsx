7378198cda568600baaf9ceff4cff99c66ae4780
manuelrurda <manuelrurda@gmail.com>
2025-05-12 00:10:06 -0600

    Initial setup

---
+++ frontend/app/app/routes/home.tsx
@@ -0,0 +1,17 @@
+import NavBar from "~/components/nav-bar";
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  );
+}
