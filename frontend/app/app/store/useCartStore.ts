b37960c1c33bb5cf9d1216c003035da22069ad05
manuelrurda <manuelrurda@gmail.com>
2025-05-14 11:07:40 -0600

    Agregar elementos al carrito

---
+++ frontend/app/app/store/useCartStore.ts
@@ -0,0 +1,62 @@
+import type { Product } from "@/lib/types";
+import { create } from "zustand";
+
+interface ProductWithAmount extends Product {
+  amount: number;
+}
+
+interface CartState {
+  products: ProductWithAmount[];
+  getProduct: (product: Product) => ProductWithAmount | undefined;
+  addProduct: (product: Product) => void;
+  removeProduct: (product: Product) => void;
+  emptyCart: () => void;
+  totalPrice: () => number;
+}
+
+const useCartStore = create<CartState>((set, get) => ({
+  products: [],
+  getProduct: (product) =>
+    get().products.find((item) => item.id === product.id),
+  addProduct: (product) => {
+    const itemExists = get().products.find((item) => item.id === product.id);
+
+    if (itemExists) {
+      set((state) => ({
+        products: state.products.map((item) =>
+          item.id === product.id
+            ? {
+                ...item,
+                amount: item.amount + 1,
+              }
+            : item
+        ),
+      }));
+    } else {
+      set((state) => ({
+        products: [...state.products, { ...product, amount: 1 }],
+      }));
+    }
+  },
+  removeProduct: (product) => {
+    const itemExists = get().products.find((item) => item.id === product.id);
+    if (!itemExists) return;
+
+    if (itemExists.amount === 1) {
+      set((state) => ({
+        products: state.products.filter((item) => item.id !== product.id),
+      }));
+    } else {
+      set((state) => ({
+        products: state.products.map((item) =>
+          item.id === product.id ? { ...item, amount: item.amount - 1 } : item
+        ),
+      }));
+    }
+  },
+  emptyCart: () => set((state) => ({ products: [] })),
+  totalPrice: () =>
+    get().products.reduce((sum, item) => sum + item.price * item.amount, 0),
+}));
+
+export default useCartStore;
