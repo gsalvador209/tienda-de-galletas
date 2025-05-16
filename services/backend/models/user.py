49885edf6fc20f1fbf718c66f52ce0be99da84a3
Salvador Chavez <dchavezvillanueva@gmail.com>
2025-05-15 19:21:57 -0600

    fix(user): fix user created_at property

--- services/backend/models/user.py
+++ services/backend/models/user.py
@@ -11,7 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(150), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=True)
     role = db.Column(db.String(50), default='user') #Pendiente si vale la pena porque aun no sabemos si haremos 2 vistas
-    created_at = db.Column(db.DateTime, default=datetime.now()
+    created_at = db.Column(db.DateTime, default=datetime.now())
 
     def to_dict(self):
         return {
