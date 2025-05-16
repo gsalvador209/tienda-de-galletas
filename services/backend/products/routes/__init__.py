91be87801788832ce662a636a074959669a39e4c
Salvador Chavez <dchavezvillanueva@gmail.com>
2025-05-15 19:54:12 -0600

    feature(api): implement PUT and DELETE methods

--- services/backend/products/routes/__init__.py
+++ services/backend/products/routes/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort
 from models.product import Product, db
 
 products_bp = Blueprint('products', __name__)
@@ -17,4 +17,40 @@ def create_product():
     db.session.commit()
     return jsonify(p.to_dict()), 201
 
-#Poner el PUT y el DELETE
+@products_bp.route('/<init:id>', methods = ['PUT'])
+def update_product(id):
+    """
+    Actualiza un producto existente.
+    """
+    product = Product.query.get(id)
+    if not product:
+        abort(404, description= f"El producto con id {id} no fue encontrado")
+    
+    data = request.get_json() or {}
+
+    updatable_fields = ['name', 'description', 'price', 'stock', 'image_url']
+    for field in updatable_fields:
+        if field in data:
+            setattr(product, field, data[field])
+    
+    db.session.commit()
+
+    return jsonify(product.to_dict()), 200
+
+@products_bp.route('/<int:id>', methods = ['DELETE'])
+def delete_product(id):
+    """
+    Elimina un producto existente.
+    """
+    product = Product.query.get(id)
+    if not product:
+        abort(404, description= f"El producto con id {id} no fue encontrado")
+    
+    db.session.delete(product)
+
+    db.session.commit()
+
+    return '', 204
+
+
+
