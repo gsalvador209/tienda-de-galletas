2bdfc62b0aa1ce4e1235846dd74ae807e71a6958
mirnmarq <mirdanmar13@gmail.com>
2025-05-17 12:04:42 -0600

    Create users.sql

---
+++ database/seeds/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (auth0_id, email, name, role)
+VALUES
+  ('auth0|user1', 'cliente1@example.com', 'Cliente Uno', 'cliente'),
+  ('auth0|user2', 'admin@example.com', 'Administrador', 'admin');
