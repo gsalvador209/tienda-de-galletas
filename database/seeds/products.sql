7895e7295f51b7736c81c955c1da8a9dd3977948
mirnmarq <mirdanmar13@gmail.com>
2025-05-17 12:05:15 -0600

    Create products.sql

---
+++ database/seeds/products.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (name, description, price, stock)
+VALUES
+  ('Galleta de Chocolate', 'Deliciosa galleta con chispas de chocolate', 15.50, 100),
+  ('Galleta de Avena', 'Galleta saludable de avena y miel', 12.00, 80),
+  ('Galleta de Mantequilla', 'Clásica galleta de mantequilla', 10.00, 120);
