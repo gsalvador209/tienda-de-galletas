e8ab6c7ba185b9f69d5e6be871f81948e7c4581e
manuelrurda <manuelrurda@gmail.com>
2025-05-17 17:36:18 -0600

    fix(user): Update user model, replace oauth with password field

--- services/backend/models/user.py
+++ services/backend/models/user.py
@@ -7,7 +7,7 @@ class User(db.Model):
     __tablename__ = 'users' # Tabla de usuarios “local”
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    auth0_id = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=True)
     role = db.Column(db.String(50), default='user') #Pendiente si vale la pena porque aun no sabemos si haremos 2 vistas
@@ -16,7 +16,6 @@ class User(db.Model):
     def to_dict(self):
         return {
             'id': self.id,
-            'auth0_id': self.auth0_id,
             'email': self.email,
             'name': self.name,
             'role': self.role,
