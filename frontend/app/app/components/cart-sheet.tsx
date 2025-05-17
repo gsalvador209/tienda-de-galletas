e17484bb933f2b034ff25bcf913cef0527d92f05
eddspicy <eduardo.cervantes@fi.unam.edu>
2025-05-17 12:57:27 -0600

    feat:cartItem terminado, junto a implemenatción de  scroll en CartSheet, mostrar total y botón de checkout

--- frontend/app/app/components/cart-sheet.tsx
+++ frontend/app/app/components/cart-sheet.tsx
@@ -13,18 +13,32 @@ const CartSheet = () => { //Componente funcional, pieza reutilizable de interfaz
       <SheetTrigger> 
          <CartSheetTrigger />
       </SheetTrigger>
-      <SheetContent>
-        <SheetHeader className="font-bold text-lg">
+      <SheetContent className="flex flex-col h-full">
+        <SheetHeader className="font-bold text-lg pb-4 border-b">
           <Logo />
+          <p className="font-bold text-center text-gray10000 mb-1">En tu carrito</p>
         </SheetHeader>
-        {cart.products.map((product) => (
-          <CartItem
-                key={product.id} 
-                product={product} 
-              />
-
-        ))
-      }
+        <div className="flex-grow overflow-y-auto py-4">
+          {cart.products.map((product) => (
+              <div key={product.id} className="px-4 mb-3">
+                <CartItem
+                  product={product} 
+                />
+              </div>
+            ))
+          }
+        </div>
+        {cart.products.length > 0 && (
+          <div className="border-t mt-auto p-4"> {/* mt-auto lo empuja hacia abajo, border-t para una línea, p-4 para padding */}
+            <div className="flex justify-between font-semibold text-lg mb-4">
+              <span>Total:</span>
+              <span>{formatCurrency(cart.totalPrice())}</span>
+            </div>
+            <button className="w-full bg-rose-300 hover:bg-pink-300 text-white py-2 rounded-lg"> {/* Botón simple de ejemplo */}
+              Checkout
+            </button>
+          </div>
+        )}
       </SheetContent>
     </Sheet>
   );
